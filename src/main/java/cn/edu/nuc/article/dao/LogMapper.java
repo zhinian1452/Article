f014eca9ffed807dfccf8e9ed29008483c1ec668
lichongyang <2308247015@qq.com>
2019-11-27 15:38:20 +0800

    frist commit

---
+++ src/main/java/cn/edu/nuc/article/dao/LogMapper.java
@@ -0,0 +1,29 @@
+package cn.edu.nuc.article.dao;
+
+import java.util.List;
+
+import cn.edu.nuc.article.entity.Log;
+
+/**
+ * 日志Mapper
+ *
+ * @author 李重阳
+ */
+public interface LogMapper {
+
+    /**
+     * 选择性插入
+     *
+     * @param record
+     * @return
+     */
+    int insertSelective(Log record);
+
+    /**
+     * 模糊查询
+     *
+     * @param log
+     * @return
+     */
+    List<Log> selectByKeyword(Log log);
+}
